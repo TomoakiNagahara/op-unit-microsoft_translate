16390d06df2ab210040a840383bcc51836eea3a6
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-06 15:28:24 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/** op-unit-microsoft_translate:/index.php
+ *
+ * @created    2024-01-06
+ * @version    1.0
+ * @package    op-unit-microsoft_translate
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+
+//	...
+include('Microsoft_Translate.class.php');
