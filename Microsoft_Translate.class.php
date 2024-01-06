cac253fb333d4eab9327937256364cddf067bf7c
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-06 15:30:46 +0900

    New: Microsoft_Translate :: Fetch()

--- Microsoft_Translate.class.php
+++ Microsoft_Translate.class.php
@@ -61,4 +61,54 @@ class Microsoft_Translate implements IF_UNIT
 		//	...
 		return json_decode($json, true)['translation'];
 	}
+
+	/** Fetch from Microsoft translate API.
+	 *
+	 * @created 2024-01-07
+	 * @param   array       $strings
+	 * @param   string      $to_lang
+	 * @param   string      $from_lang
+	 * @return  void|string
+	 */
+	static function Fetch(array $strings, string $to_lang, string $from_lang=null)
+	{
+		//	...
+		$config = OP()->Config('microsoft_translate');
+		$region = $config['region'] ?? null;
+		$apikey = $config['Ocp-Apim-Subscription-Key'] ?? null;
+
+		//	Secret API key
+		if(!$apikey ){
+			self::Errors("Config('Ocp-Apim-Subscription-Key') is empty.");
+			return;
+		}
+
+		//	Text format
+		$txttype = 'html';
+
+		//	Build URL Query
+		$query = [];
+		$query[] = "api-version=3.0";
+		$query[] = "to={$to_lang}";
+		$query[] = "textType={$txttype}";
+		$query = join('&', $query);
+
+		//	Build request strings.
+		$text = [];
+		foreach( $strings as $string ){
+			$string = addslashes($string);
+			$text[] = "{'Text':'{$string}'}";
+		}
+		$text = join(',', $text);
+
+		//	...
+		$json = `curl -X POST "https://api.cognitive.microsofttranslator.com/translate?{$query}" \
+		-H "Ocp-Apim-Subscription-Region: {$region}" \
+		-H "Ocp-Apim-Subscription-Key: {$apikey}" \
+		-H "Content-Type: application/json; charset=UTF-8" \
+		-d "[{$text}]"`;
+
+		//	...
+		return $json;
+	}
 }
