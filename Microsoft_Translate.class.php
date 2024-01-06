fdb0eb91be7911dc1bb200f05b4c182931eb71f3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2024-01-06 15:29:04 +0900

    New: Microsoft_Translate.class.php

---
+++ Microsoft_Translate.class.php
@@ -0,0 +1,37 @@
+<?php
+/** op-unit-microsoft_translate:/MicrosoftTranslate.class.php
+ *
+ * @created    2024-01-05
+ * @version    1.0
+ * @package    op-unit-microsoft_translate
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT;
+
+/** use
+ *
+ */
+use OP\IF_UNIT;
+use OP\OP_CORE;
+use OP\OP_CI;
+
+/** Microsoft
+ *
+ */
+class Microsoft_Translate implements IF_UNIT
+{
+	/** use
+	 *
+	 */
+	use OP_CORE, OP_CI;
+}
