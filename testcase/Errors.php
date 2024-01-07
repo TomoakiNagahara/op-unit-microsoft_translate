de1e8889dd19118a6a156049edd5d8ff39c950a2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2024-01-08 00:02:51 +0900

    New: testcase > Errors.php

---
+++ testcase/Errors.php
@@ -0,0 +1,27 @@
+<?php
+/** op-unit-microsoft_translate:/testcase/Errors.php
+ *
+ * @created    2024-01-08
+ * @version    1.0
+ * @package    op-unit-microsoft_translate
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+ /** Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\Microsoft_Translate;
+
+/** use
+ *
+ */
+
+//	...
+OP()->Notice('Notice message 1');
+OP()->Notice('Notice message 2');
